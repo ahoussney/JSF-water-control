e03ee52e4a7f749a591056345c79be89764823a7
nateGeorge <wordsforthewise@gmail.com>
2016-08-04 17:00:54 -0600

    feat: added pump control button and pump status, config loading

--- phonegap/JSF-control/www/js/particle-funcs.js
+++ phonegap/JSF-control/www/js/particle-funcs.js
@@ -1,12 +1,17 @@
-var token = "<access_token>";
-var deviceID = "<device_id>";
-var eventPrefix = "<prefix>";
-var variable = "pressure";
+var token, deviceID, eventPrefix
 var eventStr;
 
+$.getJSON("config.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+    token = json["access_token"];
+    deviceID = json["deviceID"];
+    eventPrefix = json["eventPrefix"];
+
+    pumpStatus();
+});
 
 function getPressure() {
-
+  variable = "pressure";
   var requestURL = "https://api.spark.io/v1/devices/" + deviceID + "/" + variable + "?access_token=" + token;
   $.get( requestURL,
           {},
@@ -69,25 +74,40 @@ function switchPump() {
   $.post( requestURL,
           { name: eventPrefix, data: eventStr, access_token: token },
           function(data) {
-              /*if (data.return_value==1) {
-                  $("#result").text("Successfully switched pump").fadeTo(1000,1);
-                  $("#result").delay(1000).fadeTo(500, 0,
-                  function () {
-                      $(this).delay(500);
-                      $(this).html('');
-                      $(this).fadeTo(500, 1);
-                  });
-              } else {
-                  $("#result").text("Failed to switch pump").fadeTo(500,1);
-                  $("#result").delay(1000).fadeTo(500, 0,
-                  function () {
-                      $(this).delay(500);
-                      $(this).html('');
-                      $(this).fadeTo(500, 1);
-                  });
-              }*/
               console.log(data);
           });
 }
 
-var interval = setInterval(getPressure, 2000);
+function fillTank(minsOn) {
+  eventStr = 'turn on pump';
+  var requestURL = "https://api.spark.io/v1/devices/events";
+  $.post( requestURL,
+          { name: eventPrefix, data: eventStr, access_token: token },
+          function(data) {
+              console.log(data);
+          });
+  setTimeout(pumpOff, minsOn*60*1000);
+}
+
+function pumpOff() {
+  eventStr = 'turn off pump';
+  var requestURL = "https://api.spark.io/v1/devices/events";
+  $.post( requestURL,
+          { name: eventPrefix, data: eventStr, access_token: token },
+          function(data) {
+              console.log(data);
+          });
+}
+
+function pumpStatus() {
+  variable = "pumpStatus";
+  var requestURL = "https://api.spark.io/v1/devices/" + deviceID + "/" + variable + "?access_token=" + token;
+  $.get( requestURL,
+          {},
+          function(data) {
+              console.log(data);
+              $("#pumpStatus").html(data.result);
+          });
+}
+
+var interval = setInterval(pumpStatus, 2000);
